24de70582ccc543a51c924684c7008821e184da8
isa <ialmen1988@gmail.com>
2019-10-22 15:13:51 +0200

    canviado rutas de view y cambiar home laravel por TABU

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,11 +3,15 @@
 
 @section('content') 
 
-            <div class="search-container">
-                <form action="/action_page.php">
-                <input type="text" placeholder="Writte your search" name="searching">
-                <button type="submit"><i class="fa fa-search">Search now</i></button>
-                </form>
-            </div>
+<div id="fakebox-container">
+    <div class="fakebox">
+        <div class="fakebox-search-icon"></div>
+        <div id="fakebox-text">Busca en Tabu</div>
+        <input id="fakebox-input" type="search" class="form-control" autocomplete="off">
+    </div>
+</div>
+
+
+
 
 @endsection
