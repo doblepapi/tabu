877fac04afb3981eae2bf4014526c228942d54ec
Jason Carvajal Perez <jasonbnick@gmail.com>
2019-10-24 12:59:51 +0200

    &sass_estilos: Auto stash before merge of "feature/css&sass_estilos" and "origin/feature/css&sass_estilos"

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,6 +10,14 @@
         </div>
         <input id="fakebox-input" placeholder="Busca en Tabu" type="search" class="form-control" autocomplete="off">
     </div>
+@section('content')
+<div  id="fakebox-container">
+    <form method="POST" action="/searchIo">
+        {{csrf_field()}}
+        @csrf
+        <input type="text" id="fakebox-input" placeholder="Busca en Tabu" name="query" type="search" class="form-control" autocomplete="off">
+        <input type="submit" id="fakebox-input"  value="Search now"/>
+    </form>
 </div>
 
 
