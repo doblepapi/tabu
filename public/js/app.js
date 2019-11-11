ee5b68b30db93527dc8e996a0d610d8907d86f84
Nathalia <nathalia.italiano@gmail.com>
2019-11-11 10:51:51 +0100

    temporary colors for better tests

--- public/js/app.js
+++ public/js/app.js
@@ -49249,8 +49249,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/doble_papi/Documents/Tabu/Tabu/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/doble_papi/Documents/Tabu/Tabu/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\tabu\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\tabu\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
