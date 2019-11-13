d0131db62c7e5c073a4cced9182207d2d847f2a4
FactoriaF5 <andresmahanaim@gmail.com>
2019-11-13 16:03:50 +0100

    Creando pantalla de Admin empiezo a crear el Blade Admin para Artículos

--- public/js/app.js
+++ public/js/app.js
@@ -49249,8 +49249,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\tabu\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\tabu\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/tabu/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/tabu/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
