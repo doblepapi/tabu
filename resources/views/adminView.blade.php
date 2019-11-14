b45a2306c507d5eb35e71026404dfa1a6d421352
FactoriaF5 <andresmahanaim@gmail.com>
2019-11-14 14:29:32 +0100

    Modificadas categoria s de Administrador

--- resources/views/adminView.blade.php
+++ resources/views/adminView.blade.php
@@ -4,9 +4,8 @@
     <h1>Hola administrador !!!</h1>
 
     <button class="botonCuadrado" id="elLugar">Lugar</button>
-    <button class="botonCuadrado" id="elArticulo" type="button" onclick="window.location='{{ url("admin/article") }}'">Artículo</button>
-    <button class="botonCuadrado" id="laPalabra">Palabras</button>
+    <button class="botonCuadrado" id="elArticulo" type="button" onclick="window.location='{{ url("admin/article") }}'">Artícules</button>
+    <button class="botonCuadrado" id="losEventos">Eventos</button>
     <button class="botonCuadrado" id="laDefinicion">Definición</button>
-    <button class="botonCuadrado" id="elInteres">Intereses</button>
-    <button class="botonCuadrado" id="elAdministrador">Admin</button>
+    <button class="botonCuadrado" id="crearAdmin">Crea Admin</button>
 @endsection
