caf10c0dbee40b4a1165d24e2b7bfb96592188be
Manuela Grajales Duque <grajalesduquemanuela@gmail.com>
2019-11-14 17:18:19 +0100

    creacion del controlador, modelo y Ficheros para las vistas de Places

--- public/js/app.js
+++ public/js/app.js
@@ -49249,8 +49249,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\tabu\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\tabu\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/SpicyBanana/tabu/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/SpicyBanana/tabu/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,56 @@
 (function() {
     var map;
+    var _icon_counter = 0;
+  
+    const icons = {
+      icon1: {
+        icon: 'http://maps.google.com/mapfiles/kml/paddle/1.png',
+      },
+      icon2: {
+        icon: 'http://maps.google.com/mapfiles/kml/paddle/2.png',
+      },
+    };
+  
+    function addPointToPlace(event, icon) {
+      // Extraer latitud y longitud del punto en el mapa
+      const myLatLng = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      };
+      // Escribir mensaje de donde se va a poner el punto
+      console.log('Add point to', event.latLng.lat(), event.latLng.lng());
+      // Crear un marcador con la ubicación
+      const marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: '' + myLatLng.lat + ',' + myLatLng.lng + '',
+        animation: google.maps.Animation.DROP,
+      });
+      // cambiar el icono
+      marker.setIcon(icon);
+    }
+  
     function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 41.6867865, lng: 0.6246724 },
-            zoom: 8
-        });
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 41.6867865, lng: 0.6246724 },
+        zoom: 8,
+      });
+  
+      // agregamos un listener para el icono
+      google.maps.event.addListener(map, 'click', function(event) {
+        var icon;
+        // si el contador de icono es par:
+        if (_icon_counter % 2 === 0) {
+          icon = icons.icon1.icon;
+        } else {
+          icon = icons.icon2.icon;
+        }
+        _icon_counter += 1;
+        addPointToPlace(event, icon);
+      });
     }
-    window.initMap = function(){
-        setTimeout(initMap, 30);
-    }
-})();
+  
+    window.initMap = function() {
+      setTimeout(initMap, 30);
+    };
+  })();
