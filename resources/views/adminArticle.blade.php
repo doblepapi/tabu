2186326c782807ab9b7261c267cc0d49f5711e76
FactoriaF5 <andresmahanaim@gmail.com>
2019-11-14 16:01:24 +0100

    Creo vista introducir datos del articulo

--- resources/views/adminArticle.blade.php
+++ resources/views/adminArticle.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 @section('content')
         
-    <h1>Hola administrador y estoy creando un Artículo !!!</h1>
+    <h1>Hola administrador /Artículo !!!</h1>
 
+    <h3>Introducir aquí los datos para crear, editar o borrar un artículo</h3>
+    <form>
+    <tr><td>Id del articulo</td>&nbsp&nbsp<td><input type="text" id = "titleId" value="escribe la id del articulo si lo conoces"/></td></br>
+    <td>Titulo del articulo</td>&nbsp&nbsp<td><input type="text" id = "title" value="escribe el titulo"/></td></br>
+    <td>Resumen del artículo</td>&nbsp&nbsp<td><input type="text" id= "resumenArticulo" value="pega el resumen del artículo"/></td></br>
+    <td>Link donde encontrar artículo</td>&nbsp&nbsp<td><input type="url" id= "link" value="pega el link donde encontrar el artículo"/></td></br>
+    <td>Autor artículo</td>&nbsp&nbsp<td><input type="text" id= "autor" value="pega nombre del autor del artículo"/></td></br>
+    <td>Año</td>&nbsp&nbsp<td><input type="text" id= "año" value="pega el año del artículo"/></td></br>
+    <td>Palabras clave para la búsqueda</td>&nbsp&nbsp<td><input type="text" id= "clavesArticulos" value="pega las palabras clave que serviran a la busqueda"/></td></tr></br>
+    <button>Guardar</button>
+    <button>Editar</button>
+    <button>Borrar</button>
     
-
+    </form>
 @endsection
