161e2a57920912d4d8c17591629a65e313a036ce
Jason Carvajal Perez <jasonbnick@gmail.com>
2019-11-19 16:23:40 +0100

    El modal tiene un poco de estilo

--- resources/views/searchResult.blade.php
+++ resources/views/searchResult.blade.php
@@ -5,22 +5,25 @@
   <div id="container justify-content-md-center">
     <div class="row align-content-md-center">
       <div id="app">
-        <button id="definicion" class="col-md align-self-center card" @click="showModal = true">Show Modal</button>
+        <div id="definicion" class="col align-self-center card" @click="showModal = true">Definicion</div>
       </div>
-    <div id="lugares" class="col align-self-center card"><a href={{"/map"}}> Lugares</a></div>
-    <div id="eventos" class="col card"><a href={{"/"}}>Eventos</a></div>
-    <div id="blog" class="col card"><a href={{"/"}}>Blog</a></div>
-    <div id="articulos" class="col card"><a href={{"/"}}>Articulos</a></div>
+    <div id="lugares" class="col align-self-center card" href={{"/map"}}> Lugares</a></div>
+    <div id="eventos" class="col align-self-center card"><a href={{"/"}}>Eventos</a></div>
+    <div id="blog" class="col align-self-center card"><a href={{"/"}}>Blog</a></div>
+    <div id="articulos" class="col align-self-center card"><a href={{"/"}}>Articulos</a></div>
     </div>
   </div>
   
   <div class="buscarResultado">
       @foreach($searchResult->groupByType() as $type=>$modelSearchResults)
         @foreach($modelSearchResults as $searchResult)
-          <modal v-if="showModal" @close="showModal = false">
+          
+        <modal class="modal-content" v-if="showModal" @close="showModal = false">
             <h3 slot="header">{{ucfirst($type)}}</h3>
-            <p slot="body" href="{{$searchResult->url}}">{{$searchResult->title}}</p>     
-      </modal>    
+            <p slot="body" href="{{$searchResult->url}}">{{$searchResult->title}}</p>
+          </modal>  
+
+  </div>  
       @endforeach
     @endforeach
   </div>
