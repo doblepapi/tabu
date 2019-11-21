f9cabb71b466f643c1a9be31e1d7d191592e761a
Manuela Grajales Duque <grajalesduquemanuela@gmail.com>
2019-11-21 11:25:02 +0100

    se elimina dd en

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
         $searchResult = (new  Search())
             ->registerModel(Post::class, 'title', 'body', 'id')
             ->perform($request->input('query')); 
-            dd($searchResult);
+            //dd($searchResult);
         return view('searchResult', compact('searchResult'), ['palabra'=>$request->input('query')]);
     }
 
