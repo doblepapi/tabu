197f17d965b0ef518b375a3c9571f6d214cd93cf
Jason Carvajal Perez <jasonbnick@gmail.com>
2019-11-21 14:58:52 +0100

    cambio estilos events

--- resources/views/adminView.blade.php
+++ resources/views/adminView.blade.php
@@ -5,7 +5,7 @@
 
     <button class="botonCuadrado" id="elLugar">Lugar</button>
     <button class="botonCuadrado" id="elArticulo" type="button" onclick="window.location='{{ url("articles") }}'">Articules</button>
-    <button class="botonCuadrado" id="losEventos">Eventos</button>
+    <button class="botonCuadrado" id="losEventos" type="button" onclick="window.location='{{ url("events") }}'">Eventos</button>
     <button class="botonCuadrado" id="laDefinicion" type="button" onclick="window.location='{{ ("post.index") }}'">Definición</button>
     <button class="botonCuadrado" id="crearAdmin">Crea Admin</button>
 @endsection
