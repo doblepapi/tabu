35495dfc6599c414d8b8e107866870ff8d2d525e
Jason Carvajal Perez <jasonbnick@gmail.com>
2019-11-22 08:54:09 +0100

    correcion front events

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,10 +4,10 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Definition</h2>
+                <h2>Eventos</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('post.create') }}"> Create New Definition</a>
+                <a class="btn btn-success" href="{{ route('events.create') }}"> Create New Event</a>
             </div>
         </div>
     </div>
